3651024375b288b6d113fccd2493f77e9b1248c1
denev <denev723@gmail.com>
2021-06-11 01:45:48 +0900

    counter useselector 최적화

--- src/container/CounterContainer.js
+++ src/container/CounterContainer.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease, setDiff } from "../modules/counter";
 
 const CounterContainer = () => {
   // useSelector는 redux store의 상태를 조회하는 Hook
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  const { number, diff } = useSelector(
+    (state) => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    shallowEqual,
+  );
 
   // useDispatch는 redux store의 dispatch를 함수에서 사용할 수 있게 해주는 Hook
   const dispatch = useDispatch();
