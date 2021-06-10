57f28b43dd1b738ee4d4ca12865ff24b862a8bf4
denev <denev723@gmail.com>
2021-06-11 01:38:53 +0900

    todos component 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import CounterContainer from "./container/CounterContainer";
+import TodosContainer from "./container/TodosContainer";
 
 function App() {
   return (
     <div className="App">
       <CounterContainer />
       <hr />
+      <TodosContainer />
     </div>
   );
 }
